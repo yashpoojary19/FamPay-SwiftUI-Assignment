4ee67a23e82a3f54e5ba150700877b30aaa14c57
Yash Poojary <yash@capshun.co>
2022-06-28 18:58:59 +0530

    Initial Commit

---
+++ Fampay-SwiftUI/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  FamPay-SwifUI
+//
+//  Created by Yash Poojary on 28/06/22.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World!")
+    }
+}
+
+
